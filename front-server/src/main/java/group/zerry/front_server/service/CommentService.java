9778600bc8940b32b852375118371f68cd51e7a7
ZerryChu <zzr940809@163.com>
2015-10-22 19:21:03 +0800

    。。。

--- front-server/src/main/java/group/zerry/front_server/service/CommentService.java
+++
@@ -1,5 +0,0 @@
-package group.zerry.front_server.service;
-
-public interface CommentService {
-	public String show_message(int id);
-}
--- front-server/src/main/java/group/zerry/front_server/service/SearchService.java
+++
@@ -1,8 +0,0 @@
-package group.zerry.front_server.service;
-
-public interface SearchService {
-	public String searchUsersLikeNickname(String nickname);
-	
-	public String searchMessagesLikeContent(String content);
-
-}
